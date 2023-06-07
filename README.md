58ba98217cb96e76f3f4c5233c9c2ae02dc4279b
Roberto Fokou <robertofokou4@gmail.com>
2023-06-07 13:28:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Inscription
+# Inscription
---
+++ formulire.js
@@ -0,0 +1,31 @@
+function message(sms){
+    var messenger = document.querySelector('.message');
+    
+    var span = document.createElement('p');
+    span.textContent = sms;
+    messenger.appendChild(span);    
+}
+
+// message('veillez reessayer');
+// message('le nom doit contenir au moins 5 caracteres');
+
+
+
+var envoie = document.getElementById('send');
+
+envoie.addEventListener('click', (e)=>{
+    e.preventDefault();
+    var name = document.getElementById('username');
+    var mail = document.getElementById('email');
+    var mdp = document.getElementById('password');
+    var mdpconfirme = document.getElementById('passwordconfirm');
+
+    if(name.value.length < 5){
+        message('le username doit comporter au moins 5 carateres');
+    }
+
+    if(mdp != mdpconfirme){
+        message('les mots de passes sont differents');
+    }
+    
+})
